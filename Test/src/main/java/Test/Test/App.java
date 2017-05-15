5cf63500c56e07834c8e7b8e408cbbdf29315db4
yorktsang <yorktsang@gmail.com>
2017-05-15 23:37:47 +0800

    Update App.java

--- Test/src/main/java/Test/Test/App.java
+++ Test/src/main/java/Test/Test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldsss!" );
+        System.out.println( "Hello Worldsssdsdsdsds!" );
     }
 }
